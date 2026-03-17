5af8b32b52696674ad7bfee37f6da5f525745dbd
Meluwor <mukeratov@web.de>
2026-03-17 16:50:51 +0100

    added basic Open-AI access

---
+++ open_ai.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+API_KEY = os.getenv("OPENAI_GTP_KEY")
+
+
+client = OpenAI(api_key=API_KEY)
+
+
+
+response = client.responses.create(
+    model="gpt-4o-mini",
+    input="geb mir mal eine liste der 10 beliebtesten strategiepielen und deren rating pries und publisher in form eines python dict"
+)
+
+print(response)
+print(response.output_text)
+"""
+for key in response:
+    print(key)
+    pass
+
+"""
+
+
+
+
+
+def main():
+    print("start")
+
+if "__name__" == "_main_":
+    main()
